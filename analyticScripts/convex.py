f19fb1992b840409e9edebe643e0d2928f2d95e9
Aaron Grisez <grise100@mail.chapman.edu>
2018-06-22 12:20:14 -0400

    Refactored some code and added new numerics

--- analyticScripts/convex.py
+++ analyticScripts/convex.py
@@ -1,12 +1,93 @@
-import cvxopt
+import cvxpy as cvx
+import numpy as np
 import utilities
+import numba as nb
+import matplotlib.pyplot as plt
+import psutil
+import time
 
-def optimization(n, r1, r2, theta):
+
+@nb.jit
+def optimize(n, r1, r2, theta):
     """
-    Returns max(tr(W)) st. rho-W>0 and sigma-W>0
+    For a given qubit pair, optimize Tr(W) where rho-W>0 and sigma-W>0
     """
-    c = cvxopt.matrix([1. for i in range(n)])
+    dim = 2 ** n
+    W = cvx.Variable((dim, dim), PSD=True)
+    zeros = np.matrix(np.zeros((dim, dim)))
     rho, sigma = utilities.setUpN2QubitSystems(n, r1, r2, theta)
-    #G = [cvxopt.matrix([[1.] for i in range(n*4)]) for i in range(2)]
-    h = [cvxopt.matrix(rho), cvxopt.matrix(sigma)]
-    sol = cvxopt.solvers.sdp(c, Hl=h)
+    mem = psutil.virtual_memory()
+    print("States Generated. n = " + str(n) + ": " + str(mem.used) + " used, " + str(mem.available) + " available")
+    print("Time: " + time.asctime( time.localtime(time.time())))
+    obj = cvx.Maximize(1/2 * cvx.trace(W))
+    constraints = [rho - W >= 0, sigma - W >=0]
+    mem = psutil.virtual_memory()
+    print("Constraints set. : " + str(mem.used) + " used, " + str(mem.available) + " available")
+    p = cvx.Problem(obj, constraints) 
+    mem = psutil.virtual_memory()
+    print("Problem set. : " + str(mem.used) + " used, " + str(mem.available) + " available")
+    return p.solve()
+
+@nb.jit
+def mSequenceOptimize(m, r1, r2, theta, l):
+    """
+    For a given qubit pair, get sequence of optimizations for n<=m
+    Tested for m <= 9, runtime reasonable
+    l is starting point
+    """
+    sequence = np.zeros(m-1-l)
+    for (j,i) in enumerate(range(1+l, m)):
+        sequence[j] = optimize(i, r1, r2, theta)
+        print("Time: " + time.asctime( time.localtime(time.time())))
+        mem = psutil.virtual_memory()
+        print("Optimum reached. : " + str(mem.used) + " used, " + str(mem.available) + " available")
+    return sequence
+
+@nb.jit
+def mSequenceOptimizeLog(m, r1, r2, theta):
+    """
+    For a given qubit pair, get sequence of optimizations for n<=m
+    Tested for m <= 9, runtime reasonable
+    """
+    sequence = np.zeros(m-1)
+    for i in range(1, m):
+        sequence[i-1] = -np.log(optimize(i, r1, r2, theta)) / i
+    return sequence
+
+def plotMSequenceOptimize(m, r1, r2, theta, log=False):
+    """
+    For a given qubit pair, plot mSequence
+    """
+    n_vals = [i for i in range(1, m)]
+    if log == False:
+        sequence = mSequenceOptimize(m, r1, r2, theta)
+    else:
+        sequence = mSequenceOptimizeLog(m, r1, r2, theta)
+        #This next line makes the assumption that r1=r2 to give minimum value of s for QCB
+        qcb = utilities.qChernoffInformation(r1, r2, theta, 1/2)
+        plt.hlines(qcb, 0, m+.5)
+    plt.scatter(n_vals, sequence, c='black', s=.6)
+
+@nb.jit
+def scanThetaOptimize(max_systems, r1, r2, theta_min, theta_max, steps):
+    """
+    Scans theta parameter and returns the mSequence of convex optimization problem solutions for the two states
+
+    r1 - Radius of first qubit
+    r2 - Radius of second qubit
+    max_systems - Number of copies of qubit systems
+    """
+    tvals = np.linspace(theta_min, theta_max, steps)
+    solutions = np.zeros((steps, max_systems-1))
+    for (i, j) in enumerate(tvals):
+        solutions[i,:] = mSequenceOptimize(max_systems, r1, r2, j)
+    return [tvals, solutions]
+
+def plotScanThetaOptimize(max_systems, r1, r2, theta_min=1.0E-30, theta_max=180, steps=100):
+    """
+    Plots theta parameter scan for optimization problem
+    """
+    tvals, x = scanThetaOptimize(max_systems, r1, r2, theta_min, theta_max, steps)
+    for i in range(x.shape[1]):
+        for j in range(x.shape[0]):
+            plt.scatter(tvals[j], x[j,i], c='black', s=.5)
--- analyticScripts/utilities.py
+++ analyticScripts/utilities.py
@@ -6,48 +6,8 @@ from scipy.linalg import sqrtm, inv, logm, expm
 from sympy.physics.quantum import TensorProduct
 
 """
-Symbolically Calculates tr(x^t*y^(1-t)) in integral representation without performing the actual integral
+Contains variety of utility functions
 """
-
-def CBexp(a, b):
-    """
-    Takes 2 square matrices of entries, returns trace of the Chernoff Quantity
-    """
-    t = sympy.Symbol('t') #Parameter to be integrated over
-    p = sympy.Symbol('p') #Power to be minimized over
-    A = sympy.Matrix(a)
-    B = sympy.Matrix(b)
-    (dim, dim) = A.shape
-    I = sympy.eye(dim)
-    integrand1 = sympy.Matrix((t ** (1 / p) * I) + A)
-    integrand2 = sympy.Matrix((t ** (1 / (1 - p)) * I) + B)
-    total_integrand = A * integrand1 ** (-1) * B * integrand2 ** (-1)
-    trace = sympy.Integer(0)
-    for i in range(dim):
-        trace += total_integrand[i,i]
-    return trace
-
-def testQubit(r1=None, r2=None, theta=None):
-    """
-    Takes parameters for a given 2 qubit system, constructs density matrix, and plots first and second derivatives of CB_exp
-    """
-    if r1 == None:
-        r1 = sympy.Symbol("r_1")
-    if r2 == None:
-        r2 = sympy.Symbol("r_2")
-    if theta == None:
-        theta_sym = sympy.abc.theta
-    A, B = setUp2Qubits(r1, r2, theta_sym)
-    trace = CBexp(A,B)
-    return trace
-
-def setUp2QubitsSymbol(r1, r2, theta):
-    """
-    Returns density matrix representations of the two qubit states as sympy matrices
-    """
-    A = sympy.Rational(1 / 2) * sympy.Matrix([[1 + r1, 0], [0, 1 - r1]])
-    B = sympy.Rational(1 / 2) * sympy.Matrix([[1 + r1 * sympy.sin(theta * np.pi / 360), r2 * sympy.cos(theta)],[r2 * sympy.cos(theta * np.pi / 360), 1 - r2 * sympy.sin(theta * np.pi / 360)]])
-    return (A, B)
 
 @nb.jit
 def setUp2Qubits(r1, r2, theta):
@@ -64,43 +24,23 @@ def setUpN2QubitSystems(n, r1, r2, theta):
     A = A_not
     B = B_not
     if n != 1:
-        for i in range(n):
+        for i in range(n-1):
             A = np.kron(A, A_not)
             B = np.kron(B, B_not)
-
     return (A, B)
-
-@nb.jit
-def compareMatsumotoLeifer(systems, r1, r2, theta_min=1.0E-30, theta_max=360, steps=1000):
-    """
-    Scans theta parameter and returns the Leifer and Matsumoto reverse fidelities for the two states
-
-    r1 - Radius of first qubit
-    r2 - Radius of second qubit
-    systems - Number of copies of qubit systems
-    """
-    tvals = np.linspace(theta_min, theta_max, steps)
-    matsumoto = np.zeros(steps)
-    leifer = np.zeros(steps)
-    for (i, j) in enumerate(tvals):
-        states = setUpN2QubitSystems(systems, r1, r2, j)
-        matsumoto[i] = matsumotoReverseFidelity(states[0],states[1])
-        leifer[i] = leiferReverseFidelity(states[0],states[1])
-    return (matsumoto, leifer)
-
 
 def qChernoffInformation(r1, r2, theta, s):
     """
-    Returns non-logarithmic, unminimized variety of q. chernoff info as derived by Calsamiglia et al
+    Returns unminimized variety of q for qubit. chernoff info as derived by Calsamiglia et al
 
-    Can be done ussympy.integrate(integrand, (t, 0, sympy.oo))ing SymPy if arguments are passed as symbols
+    In the case of r1=r2, this expression is minimized when s = 1/2
     """
     lambda_0 = (1 + r1) / 2
     lambda_1 = (1 + r2) / 2
     return ((lambda_0 ** s * lambda_1 ** (1 - s) +
-            (1 - lambda_0) ** s * (1 - lambda_1) ** (1 - s)) * (sympy.cos(theta)) ** 2 +
+            (1 - lambda_0) ** s * (1 - lambda_1) ** (1 - s)) * (np.cos(theta/2)) ** 2 +
             (lambda_0 ** s * (1 - lambda_1) ** (1 - s) +
-            (1 - lambda_0) ** s * lambda_1 ** (1 - s)) * (sympy.sin(theta)) ** 2)
+            (1 - lambda_0) ** s * lambda_1 ** (1 - s)) * (np.sin(theta/2)) ** 2)
 
 def sDerivQChernoffInformation(r1=sympy.Symbol('r_1'), r2=sympy.Symbol('r_2'), theta=sympy.abc.theta, asUfunc = False):
     """
@@ -151,30 +91,6 @@ def setUpNQudits(n, d):
         trace = density_matrix.trace()
         rho.append(density_matrix) 
     return rho
-
-@nb.jit
-def matsumotoReverseFidelity(rho, sigma):
-    """
-    Returns Matsumoto's value for reverse fidelity, trace(rho*sqrt(rho**-1/2*sigma*rho**-1/2))
-    """
-    a = np.matrix(rho)
-    b = np.matrix(sigma)
-    c = np.matmul(inv(sqrtm(a)), b) #c = rho**(-1/2)*sigma
-    d = np.matmul(c, inv(sqrtm(a))) #d = c*rho**(-1/2)
-    e = np.matmul(a, sqrtm(d)) #e = a * sqrt(d)
-    return np.trace(e)
-
-@nb.jit
-def leiferReverseFidelity(rho, sigma):
-    """
-    Returns Leifer's value for reverse fidelity, trace(exp(1/2 * (log(rho) + log(sigma))))
-    """
-    a = np.matrix(rho)
-    b = np.matrix(sigma)
-    c = logm(a)
-    d = logm(b)
-    e = expm(1/2 * (c + d))
-    return np.trace(e)
 
 def setUpPOVMElements(d):
     """
