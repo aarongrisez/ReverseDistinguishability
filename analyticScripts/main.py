e428307f3937b9432e78f95996a79b95187bb2ef
Aaron Grisez <grise100@mail.chapman.edu>
2018-06-25 18:46:53 -0400

    Added file saving

--- analyticScripts/main.py
+++ analyticScripts/main.py
@@ -1,16 +1,20 @@
 import traceDistanceCVXOPT as opt
+import hashlib
 from tempfile import TemporaryFile
 import numpy as np
+
+DATA_PATH = './Data/'
+LOG_PATH = './Log/'
 
 def setUpParameterSpace(r_min=.1, r_max=.9, r_steps=5, t_min=.1, t_max=359, t_steps=10, num_threads=4):
     """
     Creates and partitions parameter space
     """
     parameter_points = r_steps * t_steps #Total number of points in parameter space to be tested
-    pp_per_thread = parameter_points / n #Total number of parameter points for each thread
+    pp_per_thread = parameter_points / num_threads #Total number of parameter points for each thread
     rvals = np.linspace(r_min, r_max, r_steps)
     tvals = np.linspace(t_min, t_max, t_steps)
-    parameters = np.meshgrid(rvals, tvals).reshape(-1, 2)
+    parameters = np.dstack(np.array(np.meshgrid(rvals, tvals))).reshape(-1,2)
     partition = partitionParameterSpace(parameters, num_threads, pp_per_thread)
     return partition
 
@@ -18,13 +22,13 @@ def partitionParameterSpace(parameters, num_threads, pp_per_thread):
     """
     Returns partitioned numpy array where the first index provides the thread number
     """
+    return parameters
 
 def setUpFileSystem():
     """
     Creates folders
     """
-    DATA_PATH = './Data/'
-    LOG_PATH = './Log/'
+    pass
 
 def setUpProcesses(parameters, num_threads):
     """
@@ -32,16 +36,17 @@ def setUpProcesses(parameters, num_threads):
 
     Takes the partitioned parameter space and the desired number of threads
     """
+    pass
 
 def process(params_list, depth):
     i = 0
     p_length = len(params_list)
     while i < p_length:
         chunk = []
-        while len(chunk) < 100:
+        while len(chunk) < 50:
             chunk.append(calculate(params_list[i], depth))
             i += 1
-        saveData(chunk)
+        saveData(np.array(chunk))
 
 def calculate(params_tuple, depth):
     """
@@ -56,17 +61,18 @@ def saveData(chunk):
     """
     Saves data in chunks
     """
-    outfile = TemporaryFile()
     #Write to log "saving Chunk", time, and system specs
     #Save to file
+    ID = hashlib.sha1()
+    ID.update(np.array2string(chunk).encode('utf-8'))
+    np.save(DATA_PATH + 'chunk' + ID.hexdigest() + '.npy', chunk)
 
 def execute(processes):
     """
     Executes processes
     """
+    pass
 
 if __name__ == "__main__":
-    setUpFiles()
-    params = setUpParameterSpace(r_min, r_max, r_steps, theta_min, theta_max, theta_steps, num_threads)
-    processes = setUpProcesses(params, num_threads, depth)
-    execute(processes)
+    params = setUpParameterSpace()
+    process(params, 4)
